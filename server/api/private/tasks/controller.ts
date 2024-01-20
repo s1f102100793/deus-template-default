d82f6083363025096da2afc872144b8adbc17ba4
solufa <solufa2020@gmail.com>
2024-01-21 07:53:37 +0900

    feat: remove validators

--- server/api/private/tasks/controller.ts
+++ server/api/private/tasks/controller.ts
@@ -1,26 +1,15 @@
-import { z } from 'zod';
 import { taskUseCase } from '../../../domain/task/useCase/taskUseCase';
-import { defineController, multipartFileValidator } from './$relay';
+import { defineController } from './$relay';
 
 export default defineController(() => ({
-  post: {
-    validators: {
-      body: z.object({ label: z.string(), image: multipartFileValidator().optional() }),
-    },
-    handler: async ({ user, body }) => ({
-      status: 201,
-      body: await taskUseCase.create(user, body.label, body.image),
-    }),
-  },
-  patch: {
-    validators: {
-      body: z.object({ taskId: z.string(), done: z.boolean(), label: z.string() }),
-    },
-    handler: async ({ user, body }) => {
-      const task = await taskUseCase.update(user, body.taskId, body.done, body.label);
+  post: async ({ user, body }) => ({
+    status: 201,
+    body: await taskUseCase.create(user, body.label, body.image),
+  }),
+  patch: async ({ user, body }) => {
+    const task = await taskUseCase.update(user, body.taskId, body.done, body.label);
 
-      return { status: 204, body: task };
-    },
+    return { status: 204, body: task };
   },
   delete: async ({ user, body }) => {
     await taskUseCase.delete(user, body.taskId);
